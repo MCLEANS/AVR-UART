bb5d67f3389af68252985670800fa85d0b0d6c86
MCLEANS <jackagwaya@gmail.com>
2020-05-28 19:31:54 +0300

    project bare minimum

---
+++ AVR_UART/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * AVR_UART.cpp
+ *
+ * Created: 5/28/2020 7:22:30 PM
+ * Author : MCLEANS
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
