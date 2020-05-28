324f92425c656f88650386fe6d577db118c880c7
MCLEANS <jackagwaya@gmail.com>
2020-05-28 19:47:38 +0300

    function to set uart baud rate

--- AVR_UART/main.cpp
+++ AVR_UART/main.cpp
@@ -5,13 +5,67 @@
  * Author : MCLEANS
  */ 
 
+#define F_CPU 16000000UL 
+
 #include <avr/io.h>
 #include <util/delay.h>
 
 
+void set_baudrate(uint16_t baud){
+	switch(F_CPU){
+		case 1000000UL:
+			switch(baud){
+				case 9600:
+					UBRR0L = 6;
+					break;
+				case 4800:
+					UBRR0L = 12;
+					break;
+				default:
+					break;
+				}
+		case 8000000UL:
+			switch(baud){
+				case 115200:
+					UBRR0L = 3;
+					break;
+				case 9600:
+					UBRR0L = 51;
+					break;
+				case 4800:
+					UBRR0L = 207;
+					break;
+				default:
+					break;
+			}
+		
+		case 16000000UL:
+			switch(baud){
+				case 115200:
+					 UBRR0L = 8;
+					 break;
+				case 9600:
+					UBRR0L = 103;
+					break;
+				case 4800:
+					UBRR0L = 207;
+					break;
+				default:
+					break;
+					
+			}
+		default:
+			break;
+			
+	}
+}
+
+
+
+
 int main(void)
 {
-    /* Replace with your application code */
+    
     while (1) 
     {
     }
